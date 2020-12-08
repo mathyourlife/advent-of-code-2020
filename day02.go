1e25ebd8165b0895ebad220aaf41e706e10a3059
Dan Couture <mathyourlife@gmail.com>
2020-12-07 21:56:45 -0500

    create unit test for day01 and skeleton for day02

---
+++ day02.go
@@ -0,0 +1,14 @@
+package main
+
+type Day02 struct {
+}
+
+func NewDay02() Day {
+	return &Day02{}
+}
+
+func (d *Day02) SolvePart1() {
+}
+
+func (d *Day02) SolvePart2() {
+}
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
 func main() {
+	solveDay := os.Args[1]
+	solveDayNum, _ := strconv.Atoi(solveDay)
+	fmt.Printf("solving Day %s\n", solveDay)
 	days := []DayMaker{
 		NewDay01,
+		NewDay02,
 	}
-	d := days[0]()
+	d := days[solveDayNum-1]()
 	d.SolvePart1()
 	d.SolvePart2()
 }
